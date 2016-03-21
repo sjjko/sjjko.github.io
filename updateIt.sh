1945f90131ef3daa4f90b0e41b59050f3f8162e0
sjjk <stefan.konzett@gmail.com>
2016-03-21 09:31:47 -0400

    c2

--- updateIt.sh
+++ updateIt.sh
@@ -1,6 +1,11 @@
 #echo "first we fetch everything from github"
 #git clone https://github.com/sjjko/sjjko.github.ioCloning into '.'
 echo "copy the generated html pages from _site folder in ../../gh-pages!"
+HP_FOLDER=/home/kos/Code/kos/html/gh-pages
+SUBMIT_FOLDER=$PWD
+cd $HP_FOLDER
+jekyll build
+cd $SUBMIT_FOLDER
 cp -r ../../gh-pages/_site/* ./
 echo "add everything new to git repo"
 git add -A
