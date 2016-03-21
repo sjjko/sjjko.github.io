8baac853440fc29a752c94f123b1d00976b44d4f
sjjk <stefan.konzett@gmail.com>
2016-03-21 09:29:20 -0400

    ch2

--- updateIt.sh
+++ updateIt.sh
@@ -1,5 +1,5 @@
-echo "first we fetch everything from github"
-git clone https://github.com/sjjko/sjjko.github.ioCloning into '.'
+#echo "first we fetch everything from github"
+#git clone https://github.com/sjjko/sjjko.github.ioCloning into '.'
 echo "copy the generated html pages from _site folder in ../../gh-pages!"
 cp -r ../../gh-pages/_site/* ./
 echo "add everything new to git repo"
@@ -9,5 +9,5 @@ echo "Commit message (then press enter):"
 read COMMITMESSAGE
 git commit -m $COMMITMESSAGE
 echo "now we push the page to the repository!"
-git push origin master
-echo "finished"
+git push -u origin master
+echo "finished hp update!"
